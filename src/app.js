2a5ef9b92def9e349466ba6582a1b526152a9c32
Ahmed Dev <ahmadmujtabadev@paklogics.com>
2025-10-15 18:06:42 +0500

    fix

--- src/app.js
+++ src/app.js
@@ -9,8 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(morgan('dev'));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.use(routes);
 app.use(errorHandler);
+app.get(['/favicon.ico', '/favicon.png'], (_req, res) => res.sendStatus(204));
+
 
 export default app;
